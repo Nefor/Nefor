7b2d387a55340f50b90f0d6262fb30901c7d487b
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-11-04 22:44:48 +0200

    update SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,15 +62,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Nefor_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    authentication: 'plain',
-    user_name: 'v.v.grimm@gmail.com',
-    password: Rails.application.secrets.email_password,
-    enable_starttls_auto: true
+  host = 'nefor.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
