ccafc3193aeb70a26cf71c07fc24c78af6765bc2
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-12-02 16:00:29 +0200

    add hello-world component by hands

--- app/javascript/nefor_project/app/app.component.ts
+++ app/javascript/nefor_project/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nefor-project',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `<h1>Hello {{name}}</h1>
+  <hello-world></hello-world>`
 })
 export class AppComponent {
   name: string = 'Creator!';
