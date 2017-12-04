a574ed0d4b545ba99476f89a8b902362e3985db7
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-12-04 17:18:11 +0200

    simple form through FormsModule

--- app/javascript/nefor_project/app/app.component.ts
+++ app/javascript/nefor_project/app/app.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   template: `    
   <h1>Hello {{name}}</h1>
   <!--<reddit></reddit>-->
-  <inventory-app></inventory-app>  
+  <!--<inventory-app></inventory-app>  -->
+  <ng-book-form></ng-book-form>  
   `
 })
 export class AppComponent {
---
+++ app/javascript/nefor_project/app/ng-book/components/ng-book-form/ng-book-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ng-book-form',
+  template: `
+  <div class="ui raised segment">
+    <h2 class="ui header">Demo Form: Sku</h2>
+    <form #f="ngForm" (ngSubmit)="onSubmit(f.value)" class="ui form">
+      <div class="field">
+        <label for="skuInput">SKU</label>
+        <input type="text" id="skuInput" placeholder="SKU" name="sku" ngModel>
+      </div>
+      
+      <button type="submit" class="ui button">Submit</button>
+    </form>
+  </div>
+  `
+})
+
+export class NgBookFormComponent {
+  onSubmit(form: any): void{
+    console.log(form);
+  }
+}
