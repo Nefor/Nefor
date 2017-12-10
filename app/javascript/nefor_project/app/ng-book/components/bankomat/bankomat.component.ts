7a96339336cef746c56fcf18f1f53969bdd7a82e
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-12-10 21:50:07 +0200

    Insert money form

--- app/javascript/nefor_project/app/ng-book/components/bankomat/bankomat.component.ts
+++ app/javascript/nefor_project/app/ng-book/components/bankomat/bankomat.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
 import {BankomatService} from "../../services/bankomat.service";
-import {Observable} from "rxjs/Observable";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'bankomat',
@@ -9,16 +8,45 @@ import {Observable} from "rxjs/Observable";
   <button class="btn btn-primary" (click)="putMoneyRequest()">Put Money</button>
   <button class="btn btn-primary" (click)="takeMoneyRequest()">Take Money Back</button>
   <button class="btn btn-primary" (click)="showBalance()">Get Balance</button>
+  <div *ngIf="showPutMoneyForm" class="ui raised segment">
+    <h2 class="ui header">Insert money:</h2>
+    <form [formGroup]="myForm" (ngSubmit)="onSubmit(myForm.value)" class="ui form">
+      <div formGroupName="banknotes" class="col-sm-6">
+        <div class="field">
+          <label for="1">1 UAH</label>
+          <input type="number" placeholder="Enter count" formControlName="1" name="1">  
+        </div>
+        <div class="field" *ngFor="let denomination of UAH_BANKNOTES">
+          <label for="{{denomination}}">{{denomination}} UAH</label>
+          <input type="number" placeholder="Enter count" formControlName="{{denomination}}" name="{{denomination}}">  
+        </div>
+      </div>      
+      <div formGroupName="coins" class="col-sm-6">
+        <div class="field">
+          <label for="1">1 cent</label>
+          <input type="number" placeholder="Enter count" formControlName="1" name="1">  
+        </div>
+        <div class="field" *ngFor="let denomination of UAH_COINS">
+          <label for="{{denomination}}">{{denomination}} cent</label>
+          <input type="number" placeholder="Enter count" formControlName="{{denomination}}" name="{{denomination}}">  
+        </div>
+        <div class="field  ">
+          <button type="submit" class="ui button">Insert</button>
+        </div>
+      </div>      
+    </form>
+  
+  </div>
   
   <div *ngIf="showBalanceInfo">
     <div>Banknotes:</div>
-    <ul class="list-group">
-      <li class="col-sm-4 list-group-item"
+    <ul class="">
+      <li class="col-sm-4 "
         *ngFor="let banknote of banknotes">{{banknote[0]}} UAH : {{banknote[1]}} available</li>
     </ul>
     <div>Coins:</div>
-    <ul class="list-group">
-      <li class="col-sm-4 list-group-item"
+    <ul class="">
+      <li class="col-sm-4 "
         *ngFor="let coin of coins">{{coin[0]}} Cent : {{coin[1]}} available</li>
     </ul>
     <div>Total: {{total}} UAH
@@ -28,24 +56,49 @@ import {Observable} from "rxjs/Observable";
   `
 })
 export class BankomatComponent implements OnInit{
+  UAH_BANKNOTES = ['2', '5', '10', '20', '50', '100', '200', '500'];
+  UAH_COINS = ['2', '5', '10', '25', '50'];
   showBalanceInfo: boolean;
-  showRequestMoneyForm: boolean;
-  showPushMoneyForm: boolean;
+  showTakeMoneyForm: boolean;
+  showPutMoneyForm: boolean;
 
   banknotes: Object;
   coins: Object;
   total: any;
+  myForm: FormGroup;
+  banknotesGroup: FormGroup;
+  coinsGroup: FormGroup;
 
-  constructor(private service: BankomatService) {
+  constructor(private service: BankomatService,fb: FormBuilder) {
+    this.banknotesGroup = fb.group({
+      '1': ['']
+    });
+    for (let banknote of this.UAH_BANKNOTES) {
+      this.banknotesGroup.addControl(banknote, new FormControl(''));
+    }
+    // console.log(this.banknotesGroup);
+    this.coinsGroup = fb.group({
+      '1': ['']
+    });
+    for (let coin of this.UAH_COINS) {
+      this.coinsGroup.addControl(coin, new FormControl(''));
+    }
+    // console.log(this.coinsGroup);
+    this.myForm = fb.group({
+      banknotes: this.banknotesGroup,
+      coins: this.coinsGroup
+    });
+    console.log(this.myForm);
   }
 
   ngOnInit(): void {
     this.showBalanceInfo = false;
-    this.showRequestMoneyForm = false;
-    this.showPushMoneyForm = false;
+    this.showTakeMoneyForm = false;
+    this.showPutMoneyForm = true;
   }
 
   putMoneyRequest(): void {
+    this.showPutMoneyForm = true;
     console.log('putMoneyRequest()');
   }
 
@@ -65,5 +118,9 @@ export class BankomatComponent implements OnInit{
 
   closeInfo(): void {
     this.showBalanceInfo = false;
+  }
+
+  onSubmit(form: any): void {
+    console.log(form);
   }
 }
