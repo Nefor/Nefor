996a170717a32e3dab4812049dcb18b559d09d21
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-12-14 00:10:31 +0200

    Store

--- app/javascript/nefor_project/app/ng-book/redux/02-adjusting-reducer.ts
+++ app/javascript/nefor_project/app/ng-book/redux/02-adjusting-reducer.ts
@@ -8,13 +8,16 @@ interface Reducer<T> {
 }
 
 let reducer: Reducer<number> = (state: number, action: Action) => {
-  if (action.type === 'INCREMENT') {
-    return state + 1;
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'PLUS':
+      return state + action.payload;
+    default:
+      return state;
   }
-  if (action.type === 'DECREMENT') {
-    return state - 1;
-  }
-  return state;
 };
 
 let incrementAction: Action = { type: 'INCREMENT' };
@@ -23,3 +26,31 @@ let decrementAction: Action = { type: 'DECREMENT' };
 console.log( reducer(0, incrementAction));
 console.log( reducer(1, incrementAction));
 console.log( reducer(100, decrementAction));
+
+let plusSevenAction = { type: 'PLUS', payload: 7 };
+
+console.log( reducer(3, { type: 'PLUS', payload: 9000}) );
+
+class Store<T> {
+  private _state: T;
+
+  constructor(
+    private reducer: Reducer<T>,
+    initialState: T
+  ) {
+    this._state = initialState;
+  }
+
+  getState(): T {
+    return this._state;
+  }
+
+  dispatch(action: Action): void {
+    this._state = this.reducer(this._state, action);
+  }
+}
+
+let store = new Store<number>(reducer, 0);
+console.log(store.getState());
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState());
