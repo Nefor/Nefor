74ec546a3e0edf70c9cfe2c17c5f6c547682c018
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2017-12-14 22:50:17 +0200

    popup directive

--- app/javascript/nefor_project/app/ng-book/components/hello-world/hello-world.component.ts
+++ app/javascript/nefor_project/app/ng-book/components/hello-world/hello-world.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hello-world',
   template: `<p>Hello, people!</p>
-  <div class="ui message" popup>
+  <div class="ui message" popup message="Wow!">
     <div class="header">
       Learning Directives!
     </div>
--- app/javascript/nefor_project/app/ng-book/directives/popup.directive.ts
+++ app/javascript/nefor_project/app/ng-book/directives/popup.directive.ts
@@ -1,11 +1,17 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input} from "@angular/core";
 
 @Directive({
   selector: '[popup]'
 })
 
 export class PopupDirective {
-  constructor() {
-    console.log('Directive bound');
+  @Input() message: String;
+
+  constructor(_elementRef: ElementRef) {
+    console.log('Directive bound', _elementRef);
+  }
+
+  @HostListener('click') displayMessage(): void {
+    alert(this.message);
   }
 }
